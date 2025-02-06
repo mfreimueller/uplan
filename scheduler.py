cd2727a902b2dc687f08fe88e9d20d07b001785e
Michael Freimüller <michael@mfreimueller.com>
2025-02-06 11:32:19 +0100

    rework `scheduler.py`: - remove threading - use the new Node class to store events

--- scheduler.py
+++ scheduler.py
@@ -1,131 +1,69 @@
 from event import Event
-from weight import MultiplicationWeightCalculator
-import threading
+from node import Node
+from node_filter import ChanceFilter, EctsFilter, IncludeFilter, PriorityFilter
 import time
 
 class Scheduler:
-    _weightCalculator = MultiplicationWeightCalculator()
-    _mandatory_courses = []
+    def generate_schedules(self, events, max_depth, courses_to_include):
+        root = Node(None, None)
 
-    _generated_schedules_hashes = []
-    _generated_schedules = []
-
-    def __init__(self, mandatory_courses):
-        self._mandatory_courses = mandatory_courses
-        self._lock = threading.Lock()
-
-    def generate_schedules(self, events, max_depth):
-        threads = []
         for event in events:
-            event_thread = threading.Thread(target=self._create_and_filter_schedules, args=(event, events, max_depth))
-            event_thread.start()
-            threads.append(event_thread)
-        
-        for thread in threads:
-            thread.join()
+            node = Node(event, root)
+            root.add_node(node)
 
-        # now that we generated a list of plausible candidates, we sort them by their ECTS and the chance thereof
-        self._generated_schedules.sort(key=lambda x: x.ects() * x.total_chance() * len(x.events()), reverse=True)
+            self._populate_tree_with_event(node, event, events, max_depth)
 
-        return self._generated_schedules[:10]
+        # apply filters to cut down our tree
+        filters = [ PriorityFilter(), ChanceFilter(), EctsFilter() ]
 
-    def _create_and_filter_schedules(self, event, events, max_depth):
-        schedules = self._create_schedules_for_event(event, events, max_depth)
+        if len(courses_to_include) > 0:
+            filters.insert(0, IncludeFilter(courses_to_include))
 
-        generated_schedules_hashes = []
-        generated_schedules = []
+        for filter in filters:
+            if len(root.leaves()) < 10:
+                break
 
-        for idx in range(len(schedules)):
-            # as duplications are possible, we filter any schedules we already processed
-            hash = 0
-            for idx1 in range(1, len(schedules[idx])):
-                hash += schedules[idx][idx1].hash()
+            root = filter.apply_filter(root)
+
+        schedules = []
+
+        # build the schedules from the remaining leaves
+        for leaf in root.leaves():
+            leaf_events = [leaf.event()]
+            parent = leaf.parent()
+            while parent is not None and not parent.is_root():
+                leaf_events.append(parent.event())
+                parent = parent.parent()
             
-            if hash not in generated_schedules_hashes:
-                generated_schedules_hashes.append(hash)
+            schedules.append(Schedule(leaf_events))
 
-                # before we continue, we want to make sure that all mandatory courses are in this schedule
-                must_exclude = False
-                for course in self._mandatory_courses:
-                    # if a course id is prefixed with + or - we must in- or exclude it
-                    course_id = course[1:] if course.startswith("+") or course.startswith("-") else course
-                    course_to_exclude = course.startswith("-")
-                    group_name = "1"
-                    
-                    if course.find(":") != -1:
-                        group_name = course_id[-1]
-                        course_id = course[:-2]
+        return schedules
 
-                    found_course = False
-                    for idx2 in range(1, len(schedules[idx])):
-                        event = schedules[idx][idx2]
-                        if event.id() == course_id and event.group_name() == group_name:
-                            found_course = True
-                            break
-                    
-                    if course_to_exclude and found_course:
-                        must_exclude = True
-                        break
-                    elif not course_to_exclude and not found_course:
-                        must_exclude = True
-                        break
-                
-                if not must_exclude:
-                    # [1:] to skip the calculated chance at index 0
-                    generated_schedules.append(Schedule(schedules[idx][1:]))
-        
-        # we are done with filtering the usable schedules. Now we need to add them to the global pool
-        while not self._lock.acquire(blocking=False):
-            time.sleep(1)
-            
-        for schedule in generated_schedules:
-            if schedule.hash() not in self._generated_schedules_hashes:
-                self._generated_schedules_hashes.append(schedule.hash())
-                self._generated_schedules.append(schedule)
-
-        self._lock.release()
-
-    def _create_schedules_for_event(self, event, all_events, max_depth, parents = [], depth = 1, parent_weight = 0):
-        if depth > max_depth:
-            return []
-
+    def _populate_tree_with_event(self, node, event, all_events, max_depth, depth = 1):
         filtered_events = []
         for event2 in all_events:
-            if event.id() == event2.id() or event.module_id() == event2.module_id():
+            # remove any duplicates
+            if event.id() == event2.id() or event.module_id() == event2.module_id(): # TODO: allow for similar courses from module somehow
                 continue
+            # remove any events that collide with `event`
             elif event.is_overlapping(event2):
                 continue
-            
-            colliding = False
-            for parent in parents:
-                if event2.is_overlapping(parent):
-                    colliding = True
-                    break
+            # remove any events that collide with any parent events
+            elif node.find(fn=lambda n: n.event().is_overlapping(event2)):
+                continue
+            # remove any events that are already stored above in the tree
+            elif node.find(fn=lambda n: n.event().id() == event2.id() or n.find_ancestor(fn=lambda c: c.event().id() == event2.id())):
+                continue
 
-            if not colliding:
-                filtered_events.append(event2)
-
-        self_weight = parent_weight + (event.chance() * event.priority())
+            filtered_events.append(event2)
 
         schedules = []
         for event2 in filtered_events:
-            generated_schedules = self._create_schedules_for_event(event2, filtered_events, max_depth, parents + [event], depth + 1, self_weight)
+            new_node = Node(event2, node)
+            node.add_node(new_node)
 
-            for schedule in generated_schedules:
-                schedule.append(event)
-
-                # if we are back in the first layer, we add the depth of the schedule
-                # to the weight
-                if depth == 1:
-                    schedule[0] = schedule[0] * (len(schedule) - 1) / max_depth
-
-                schedules.append(schedule)
-
-        if len(schedules) == 0:
-            return [[self_weight, event]]
-        else:
-            return schedules
-
+            if depth + 1 <= max_depth:
+                self._populate_tree_with_event(new_node, event2, filtered_events, max_depth, depth + 1)
 
 class Schedule:
     _events = []
